e6c304baf9280de68f102d67d809537b2bcbcac0
tsunshi <57198088+tsunshi@users.noreply.github.com>
2020-03-02 21:35:27 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/websaite#">
+    <meta property="og:url" content="http://itsmysite.php.xdomain.jp/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="TSUBASA TANAKA ｜ PLOFIRE and PORTFOLIO web site." />
+    <meta property="og:description" content="田中 翼 のポートフォリオサイトです。コーディングをメインに独学でWEBの学習を進めてきました。主に転職活動に活用するために当サイトを制作しました。" />
+    <meta property="og:site_name" content="田中 翼 のポートフォリオ" />
+    <meta property="og:image" content="http://itsmysite.php.xdomain.jp/img/ogp.JPG" />
+    <meta name="twitter:card" content="Summary Card" />
+    <meta name="twitter:site" content="@zhLi6VB3AF6RLOY" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/img/icon.png">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>TANAKA TSUBASA PORTFOLIO WEB SITE</title>
+</head>
+<body>
+    <header>
+        <div id="navArea">
+            <nav>
+                <div class="nav__inner">
+                <ul class="nav__inner__list">
+                    <li class="nav__inner__list__item"><a href="#top"><span class="list-name">TOP</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#about"><span class="list-name">ABOUT</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#work"><span class="list-name">WORKS</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#skill"><span class="list-name">SKILL</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#hobby"><span class="list-name">HOBBY</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#vision"><span class="list-name">VISION</span></a></li>
+                    <li class="nav__inner__list__item"><a href="#contact"><span class="list-name">CONTACT</span></a></li>
+                </ul>
+                </div>
+            </nav>          
+            <div class="toggle_btn sp-only">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>         
+            <div id="mask"></div>         
+          </div>
+          <div class="pc-nav pc-only">
+                <ul class="pc-nav__inner__list">
+                    <li class="pc-nav__inner__list__item"><a href="#about"><span class="list-name">ABOUT</span></a></li>
+                    <li class="pc-nav__inner__list__item"><a href="#work"><span class="list-name">WORKS</span></a></li>
+                    <li class="pc-nav__inner__list__item"><a href="#skill"><span class="list-name">SKILL</span></a></li>
+                    <li class="pc-nav__inner__list__item"><a href="#hobby"><span class="list-name">HOBBY</span></a></li>
+                    <li class="pc-nav__inner__list__item"><a href="#vision"><span class="list-name">VISION</span></a></li>
+                    <li class="pc-nav__inner__list__item"><a href="#contact"><span class="list-name">CONTACT</span></a></li>
+                </ul>
+              <span class="title">TSUBASA TANAKA ｜ PLOFIRE and PORTFOLIO web site.</span>
+          </div>
+          <div class="to-top">
+            <img class="to-top-btn" src="/img/to-top-btn.png" alt="トップへ戻る">
+        </div>
+
+    </header>
+    <main>
+        <div class="top" id="top">
+            <div class="top__inner">
+                <h1 class="name-img"><img src="/img/name.png" alt="TSUBASA TANAKA"></h1>
+                <p class="lead-img"><img src="/img/lead.png" alt="PROFILE and PORTFOLIO"></p>
+            </div>
+            <div class="top-message">
+                <p class="top-message__text">
+                    はじめまして、田中 翼 です。<br>京都の田舎・京丹波町で、妻と息子と暮らしながら<br class="pc-only">独学でWebを学んでいます。<br>このWebサイトで少しでも僕のことを<br>知っていただけますと幸いです。
+                </p>
+            </div>
+        </div>
+        <section class="about category" id="about">
+            <h2 class="category__title">ABOUT</h2>
+            <div class="about__inner">
+                <figure><img class="icon" src="/img/icon.png" alt="田中翼の写真"></figure>
+                <div class="about__inner__text">
+                    <p class="lead">Webコーダー</p>
+                    <p class="about-name-img"><img src="/img/about_name.png" alt="田中 翼です"></p>
+                    <p class="about-text text">
+                        1990年生まれ。A型。一児（2歳男の子）の父。<br class="pc-only">滋賀県・信楽町に生まれ、県内の工業高校に進学。高校卒業後は大阪の大手自動車メーカーで8年半の間、エンジンの開発に関わっていました。<br>結婚・婿入りを機に京都の田舎「京丹波町」に移住し、転職。現在、週末は家業である農業を手伝いながら、農機具部品製造会社に勤めています。<br class="pc-only">好奇心旺盛な性格で、興味関心があるものに対して貪欲に学び吸収します。趣味はレザークラフトと茶道、そして愛する息子と遊ぶこと。<br>Web業界に魅力を感じ、2019年から独学でコーディングを学び始めました。                    </p>
+                    <p class="more"><a class="more-link" href="#vision">more VISION 》</a></p>
+                </div>
+            </div>
+        </section>
+        <section class="work category" id="work">
+            <h2 class="category__title">WORKS</h2>
+            <div class="work__inner">
+                <div class="work__inner__item">
+                    <div class="work__inner__item__text">
+                        <p class="work-title gillSans">TSUBASA TANAKA<br>PROFILE and PORTFOLIO<br class="sp-only"> web site.</p>
+                        <p class="more-text text pc-only">転職活動用に制作したポートフォリオサイトのLPです。私の雰囲気を感じっとっていただきやすいように、写真中心の構成にしています。</p>
+                        <p class="read-more sp-only">READ more 》</p>
+                        <table class="description">
+                            <tr><th>担当範囲</th><td>： 構成、コーディング</td></tr>
+                            <tr><th>レスポンシブ</th><td>： PC／スマートフォン</td></tr>
+                            <tr><th>使用ツール</th><td>： Adobe XD／Photoshop</td></tr>
+                            <tr><th>制作時間／期間</th><td>： 2020年2月／計 約25時間</td></tr>
+                        </table>
+                    </div>
+                    <div class="work__inner__item__img">
+                        <p class="work-img"><img src="/img/Portfolio.png" alt="ポートフォリオサイトのモックアップ"></p>
+                        <a href="#" class="btn light">このサイトです</a>
+                        <a href='https://github.com/tsunshi/My-portfolio' target="_blank" class="btn pc-only work-btn">ソースコードを見る　》</a>
+                    </div>
+                </div>
+                <div class="work__inner__item">
+                    <div class="work__inner__item__text">
+                        <p class="work-title bold letter-spacing">京都の革職人 Pitoro<br>ご提案WEBサイト（LP）</p>
+                        <p class="more-text text pc-only">革職人の知人にむけLPを制作しました。
+                            模写や練習以外で初めて制作したWEBサイトです。苦労した点はPHPによるお問い合わせフォームの実装です。当時PHPを扱ったことがありませんでしたが、調べながら進めました。</p>
+                        <p class="read-more sp-only">READ more 》</p>
+                        <table class="description">
+                            <tr><th>担当範囲</th><td>： 構成、コーディング</td></tr>
+                            <tr><th>レスポンシブ</th><td>： PC／スマートフォン</td></tr>
+                            <tr><th>使用ツール</th><td>： Adobe XD／Photoshop</td></tr>
+                            <tr><th>制作時間／期間</th><td>： 2019年11月／計 約30時間</td></tr>
+                        </table>
+                    </div>
+                    <div class="work__inner__item__img">
+                        <p class="work-img"><img src="/img/PitoroWEB.png" alt="Pitoroのwebサイトモックアップ"></p>
+                        <a href='http://tsunshi283.php.xdomain.jp' target="_blank" class="btn work-btn">WEBサイトを見る　》</a>
+                        <a href='https://github.com/tsunshi/Pitoro-Web-site' target="_blank" class="btn pc-only work-btn">ソースコードを見る　》</a>
+                    </div>
+                </div>
+                <div class="work__inner__item">
+                    <div class="work__inner__item__text">
+                        <p class="work-title bold letter-spacing">ポートフォリオ冊子<br>- レザークレフト作品集 -</p>
+                        <p class="more-text text pc-only">私が制作したレザー作品の殆どは人の手に渡る為、作品集として保管するようにしました。
+                            型紙は全てオリジナルで、イラレに起こしたものを作品画像とコンセプトと共に掲載しています。</p>
+                        <p class="read-more sp-only">READ more 》</p>
+                        <table class="description">
+                            <tr><th>担当範囲</th><td>： 企画・構成、デザイン</td></tr>
+                            <tr><th>仕様</th><td>： A5中綴じ冊子、12ページ</td></tr>
+                            <tr><th>使用ツール</th><td>： Illustrator／Photoshop</td></tr>
+                            <tr><th>制作時間／期間</th><td>： 2018年／約2ヶ月間</td></tr>
+                        </table>
+                    </div>
+                    <div class="work__inner__item__img">
+                        <p class="work-img"><img src="/img/leather_book.png" alt="レザー冊子モックアップ"></p>
+                        <a href="/pdf/leather.pdf" class="btn work-btn" target="_blank">PDFを見る　》</a>
+                        <a href="#hobby" class="btn work-btn">趣味の紹介へ　》</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="skill category" id="skill">
+            <h2 class="category__title">SKILL</h2>
+            <div class="skill__inner">
+                <div class="skill__inner__item">
+                    <div class="skills">
+                        <h3 class="skills-type">Web CODING</h3>
+                            <div class="skills-row">
+                                <p class="skills-item">HTML / CSS</p>
+                                <p class="skills-item">Javascript</p>
+                            </div>
+                            <div class="skills-row">
+                                <p class="skills-item">PHP</p>
+                                <p class="skills-item  light">Word Press</p>
+                            </div>
+                    </div>
+                    <div class="skills-text-wrapper">
+                        <p class="skills-text text">WEBサイトの静的な見た目の再現ができ、Jqueryのプラグインを使用してスライダーやスクロールアニメーションを実装できます。</p>
+                        <p class="skills-text text">お問い合わせフォームはPHPで実装可能で、Word Pressはテンプレート階層と優先順位についての理解があります。（現在MAMPのローカル環境でテーマ製作中）</p>
+                    </div>
+                </div>
+                <div class="skill__inner__item">
+                    <div class="skills">
+                        <h3 class="skills-type">APPLICATION</h3>
+                            <div class="skills-row">
+                                <span class="skills-item">Illustrator</span>
+                                <span class="skills-item">Photoshop</span>
+                            </div>
+                            <div class="skills-row">
+                                <span class="skills-item">XD</span>
+                                <span class="skills-item">VS Code</span>
+                            </div>
+                    </div>
+                    <div class="skills-text-wrapper">
+                        <p class="skills-text text">簡単なイラストの描写や書き出し、画像の色味や明るさの調整や切り抜きができます。</p>
+                        <p class="skills-text text">XDについてはデザインデータからのデータ書き出しと、デザインデータからのコーディングができます。<br>テキストエディタは"Visual Studio Code"を使用しています。</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="hobby category" id="hobby">
+            <h2 class="category__title">HOBBY</h2>
+            <div class="hobby__inner">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide swiper-slide-hobby">
+                            <div class="slide__item">
+                                <div class="title-flow">
+                                <h3 class="hobby-title">レザークラフト</h3>
+                                </div>
+                                <p class="hobby-img leather"></p>
+                                <p class="hobby-text text">型紙から作り、手縫いで仕上げます。もともと物作りが好きであったことと、周りにできる人がいないことから、レザークラフトを始めました。作品の殆どは友人や家族にむけたプレゼントで、使い手にフィットするものを作ってきました。手渡した時の驚きや喜びの表情を見るのが好きで続けています。</p>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-hobby">
+                            <div class="slide__item" data-swiper-parallax="-200">
+                                <div class="title-flow">
+                                    <h3 class="hobby-title">茶道</h3>
+                                    </div>
+                                    <p class="hobby-img sadou"></p>
+                                <p class="hobby-text text">和服が好きで「和服を着て何かをしたい！」と思い立って2年ほど前に始めたのがきっかけ。凛とした空気感に包まれながらのお稽古は、今の自分には欠かせない大切な時間です。「精神修行」と言えば堅苦しいですが、先人たちが古来より大切にしてきた”日本人の心”を感じながら精進しています。</p>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-hobby">
+                            <div class="slide__item" data-swiper-parallax="-300">
+                                <div class="title-flow">
+                                    <h3 class="hobby-title">農作業</h3>
+                                    </div>
+                                    <p class="hobby-img agri"></p>
+                                <p class="hobby-text text">稲作と丹波黒豆の栽培を兼業で行っています。本格的に農作業をし始めたのは、婿入した５年前からで、義父にイチから教わっています。<br>自分で育てた農作物は、少し美味しく感じるんですよね。</p>
+                            </div>
+                        </div>
+                        <div class="swiper-slide swiper-slide-hobby">
+                            <div class="slide__item" data-swiper-parallax="-300">
+                                <div class="title-flow">
+                                    <h3 class="hobby-title">息子</h3>
+                                    </div>
+                                    <p class="hobby-img children"></p>
+                                <p class="hobby-text text">息子との遊び、お散歩、お昼寝は毎日の楽しみの一つです。どんどん成長していく息子を見ていると、感慨深く思うと同時に、「自分も頑張らなければ！」と駆り立てられます。<br>僕にとって息子は「癒し」であり「人生の目的」であり、「遊び相手」です。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </section>
+        <section class="vision category" id="vision">
+            <h2 class="category__title">VISION</h2>
+            <div class="vision__inner">
+                <h3 class="vision-title">今までのことと<br>これからのこと</h3>
+                <div class="vision-text text">
+                webを学び始めたきっかけは、地元の牧場の社長さんにwebサイトの制作を依頼されたことです。当時は全く知識がなく受けることができなかったのですが、「自分の市場価値を高めたい」「自分にしかできないことで地域の役にたちたい」と日々悶々としていた自分にとって、「ここにスキルの伸び代があるのではないか」と考え、独学で学習をスタートさせました。<br>今後は、より本格的にweb制作に携わり、web業界で活躍できる人間になりたいと思っています。<br>そして、いずれはwebエンジニアとして成長し、自分の住む町に貢献したいと思っています。
+            </div>
+        </section>
+        <section class="contact category" id="contact">
+            <h2 class="category__title">CONTACT</h2>
+            <form action="/confirmation.php" method="post" name="myForm" class="contact__inner">
+                <div class="contact__inner__item">
+                    <p class="item-name">お名前 *</p>
+                    <input type="text" class="contact__name input" name="name" autocomplete="name" required>
+                </div>
+                <div class="contact__inner__item">
+                    <p class="item-name">メールアドレス *</p>
+                    <input type="email" class="contact__email input" name="mail" autocomplete="email" required>
+                </div>
+                <div class="contact__inner__item">
+                    <p class="item-name">お問い合わせ概要</p>
+                    <input type="text" class="contact__purpose input" name="purpose" autocomplete="purpose">
+                </div>
+                <div class="contact__inner__item">
+                    <p class="item-name">お問い合わせ本文 *</p>
+                    <textarea name="inquiry"  class="contact__content input" required></textarea>
+                </div>
+                <input class="btn" type="submit" name="submit" value="メッセージを送信する　》">
+            </form>
+
+        </section>
+
+
+    </main>
+    <footer>
+        <small class="gillSans">&copy;2020 TSUBASA TANAKA</small>
+    </footer>
+
+   <!--- Drawer Menu -->
+    <script>
+        jQuery(function($) {
+            var $nav   = $('#navArea');
+            var $btn   = $('.toggle_btn');
+            var $mask  = $('#mask');
+            var open   = 'open'; // class
+            // menu open close
+            $btn.on( 'click', function() {
+                if ( ! $nav.hasClass( open ) ) {
+                $nav.addClass( open );
+                } else {
+                $nav.removeClass( open );
+                };
+            });
+            // mask close
+            $mask.on('click', function() {
+                $nav.removeClass( open );
+
+            });
+        } );
+    </script> 
+    <!-- Initialize Swiper -->   
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: "1",
+            pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: false,
+            },
+            loop: false,
+            spaceBetween:62,
+            autoplay: {
+                delay: 6000,
+                disableOnInteraction: true,
+            },
+        });
+    </script>
+      <!-- スムーススクロール -->
+    <script>
+        $(function($){
+        $(".to-top-btn").on('click',function(){
+            $('html,body').animate({ 
+            'scrollTop': 0 
+            }, 'slow');
+        });
+        
+        $(".more-link, .btn, .nav__inner__list__item a, .pc-nav__inner__list__item a").on('click',function(){
+            var id = $(this).attr("href");
+            var position = $(id).offset().top
+            $("html,body").animate({
+                "scrollTop":position
+            },"slow");
+        
+            });
+        
+            jQuery(window).on("scroll", function($) {
+            if (jQuery(this).scrollTop() > 410) {
+                jQuery(".link,.to-top-btn,.toggle_btn").fadeIn(2000);
+            } else {
+                jQuery(".link,.to-top-btn,.toggle_btn").fadeOut(2000);
+            }
+        });
+            });
+    </script> 
+    <!-- read more -->
+    <script>
+        jQuery(function($){
+        $(".read-more").on('click',function(){
+            $(this).prev().slideDown("slow");
+            $(this).prev().css("display","block");
+            $(this).attr("class",".read-more").slideUp("slow");
+        });
+        });
+    </script>
+    <script>
+        jQuery(window).on("scroll", function($) {
+    if (jQuery(this).scrollTop() == 0) {
+        jQuery(".top__inner").fadeIn(1000);
+    } else {
+        jQuery(".top__inner").fadeOut(1000);
+            }
+    });
+</script>
+<script>
+        jQuery(window).on("scroll", function($) {
+    if (jQuery(this).scrollTop() > 400) {
+        jQuery(".top-message__text").fadeIn(1000);
+    } else {
+        jQuery(".top-message__text").fadeOut(1000);
+            }
+    });
+</script>
+
+
+    
+</body>
+</html>
